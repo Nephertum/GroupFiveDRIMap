a2abe04516299e2a2cc09ce4c37229a9c01262c4
47emily <73485964+47emily@users.noreply.github.com>
2021-11-24 18:24:56 +0000

    Edit tab on admin page fully functional

--- app.js
+++ app.js
@@ -8,15 +8,45 @@ app.use(express.json());
 app.use(express.static('body-parser'));
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors())
-//entities
+
+// Entities
 const entities = require('./entities.json');
 
 const entrances = entities.entrances;
 const corridorIndex =  entities.corridorIndex;
 const buildings = entities.buildings;
 const rooms = entities.rooms;
+const archive = entities.archive;
 
-//routes
+// Functions used in routes
+function getPlace(category, id) {
+    let searchThrough;
+    if (category === "entrance"){
+        searchThrough = entrances;
+    }
+    else if (category === "building"){
+        searchThrough = buildings;
+    }
+    else if (category === "room"){
+        searchThrough = rooms;
+    }
+    else if (category === "room"){
+        searchThrough = archive;
+    }
+    else{
+        return undefined;
+    }
+    console.log(searchThrough)
+    for (let i = 0; i < searchThrough.length; i++) {
+        if (searchThrough[i].id === id) {
+            return searchThrough[i];
+        }
+    }
+    return undefined;
+}
+
+
+// Routes
 app.get('/entities', function (req, resp) {
     resp.json([entrances, corridorIndex, buildings, rooms]);
 });
@@ -41,7 +71,7 @@ app.post('/entities/add', function (req, resp) {
         id = 'b' + rooms.length;
     }
     else{
-        resp.status(404).send('No category defined');
+        resp.status(400).send('No category defined');
     }
     let place = {
         id: id,
@@ -71,6 +101,126 @@ app.post('/entities/add', function (req, resp) {
 
     resp.set('Content-Type', 'text/html');
     const htmltext = '<html> <head> <link rel="stylesheet" href="../styles.css"></head> <body> <h1> Thanks, the new place has been added! </h1> </body> </html>';
+    resp.send(htmltext);
+});
+
+app.post('/entities/edit', function (req, resp) {
+    const id = req.body.IdOfEdit;
+    const category = req.body.category;
+    const place = getPlace(category, id);
+    if (place === undefined) {
+        resp.status(404).send('Sorry, this place was not found, check your id and category are correct!');
+        return;
+    }
+    const property = req.body.property;
+    let value = req.body.editNewValue;
+    if (property === 'location') {
+        value = JSON.parse('[' + value + ']');
+    } 
+    else if (property === 'width' || property === 'height' || property === 'focus' || property === 'minzZoom' || property === 'maxZoom') {
+        if (isNaN(value)) {
+            resp.status(400).send("Sorry we couldn't update the property as it was not entered in the correct format.");
+            return;
+        }
+        value = parseInt(value);
+    }
+    if (value === undefined) {
+        resp.status(400).send("Sorry we couldn't update the property as it was not entered in the correct format.");
+        return;
+    }
+    place[property] = value;
+    resp.set('Content-Type', 'text/html');
+    const htmltext = '<html> <head> <link rel="stylesheet" href="../styles.css"></head> <body> <h1> Thanks, the property has been updated! </h1> </body> </html>';
+    resp.send(htmltext);
+});
+
+app.post('/entities/delete', function (req, resp) {
+    const id = req.body.IdOfDelete;
+    const category = req.body.category;
+    const deleteType = req.body.deleteType
+
+    let searchThrough;
+    if (category === "entrance"){
+        searchThrough = entrances;
+    }
+    else if (category === "building"){
+        searchThrough = buildings;
+    }
+    else if (category === "room"){
+        searchThrough = rooms;
+    }
+    else{
+        resp.status(404).send("Category not found");
+    }
+    let place;
+    let placeFound = false;
+    for (let i = 0; i < searchThrough.length; i++) {
+        place = searchThrough[i]
+        if (place.id === id) {
+            if(deleteType === "archive"){
+                const archiveId = 'a' + archive.length;
+                place.id = archiveId;
+                archive.push(place)
+            }
+            searchThrough.splice(i, 1);
+            placeFound = true;
+            for(let j = i; j < searchThrough.length; j++){
+                currentId = searchThrough[j].id
+                let newIdNumber = parseInt(currentId.substring(1)) - 1
+                searchThrough[j].id = currentId.charAt(0).concat(newIdNumber.toString())    
+            }
+            break;
+        }
+    }
+    if(placeFound === false){
+        resp.status(404).send("Place not found, check id is correct and matches category");
+        return;
+    }
+    console.log(searchThrough)
+    console.log(archive)
+    resp.set('Content-Type', 'text/html');
+    const htmltext = '<html> <head> <link rel="stylesheet" href="../styles.css"></head> <body> <h1>' + place.name + ' has been removed! The id\'s of locations in the same category may have been changed.</h1> </body> </html>';
+    resp.send(htmltext);
+});
+
+app.post('/entities/restore', function (req, resp) {
+    const id = req.body.IdOfRestore;
+    let place;
+    for (let i = 0; i < archive.length; i++) {
+        if (archive[i].id === id) {
+            place = archive[i];
+            archive.splice(i, 1);
+            break;
+        }
+    }
+    if (place === undefined){
+        resp.status(404).send('Place not found, check id is correct!')
+        return;
+    }
+    const category = place.category;
+    console.log(category)
+
+    if (category === "entrance"){
+        place.id = 'e' + entrances.length
+        entrances.push(place);
+    }
+    else if (category === "room"){
+        place.id = 'r' + rooms.length
+        entrances.push(rooms);
+    }
+    else if (category === "building"){
+        place.id = 'b' + buildings.length
+        buildings.push(place);
+    }
+    else {
+        resp.status(404).send("Error finding category location belongs to.");
+        return;
+    }
+
+    console.log(archive)
+    console.log(buildings)
+    resp.set('Content-Type', 'text/html');
+    const htmltext = '<html> <head> <link rel="stylesheet" href="../styles.css"></head> <body> <h1>' + place.name + ' has been restored! Its new id is ' + place.id + '.</h1> </body> </html>';
     resp.send(htmltext);
 });
 
